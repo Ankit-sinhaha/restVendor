114eaabd9db39cac8a9075bbe1c6c625bfd8c79c
ansinha <Ankit.Sinha@amdocs.com>
2024-07-20 13:58:43 +0530

    firstcommit

---
+++ src/main/java/com/springbootproject/restCloudVendor/RestCloudVendorApplication.java
@@ -0,0 +1,13 @@
+package com.springbootproject.restCloudVendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCloudVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCloudVendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootproject/restCloudVendor/RestCloudVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootproject.restCloudVendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCloudVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
