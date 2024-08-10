cfefc3149bb55bab7c8c5dae0e16529a68804161
Ankit-sinhaha <ankitecesinha@gmail.com>
2024-08-10 21:20:12 +0530

    ApiDocumentation

--- src/main/java/com/springbootproject/restCloudVendor/RestCloudVendorApplication.java
+++ src/main/java/com/springbootproject/restCloudVendor/RestCloudVendorApplication.java
@@ -2,6 +2,7 @@ package com.springbootproject.restCloudVendor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestCloudVendorApplication {
@@ -10,4 +11,6 @@ public class RestCloudVendorApplication {
 		SpringApplication.run(RestCloudVendorApplication.class, args);
 	}
 
+
+	
 }
